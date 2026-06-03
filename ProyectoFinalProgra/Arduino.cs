bbdfeb64adb9bdbca1cc982205f4d9dd9356edbb
Lsfer <lsfer909@gmail.com>
2026-06-03 15:42:47 -0600

    1.Crear clase Arduino

---
+++ ProyectoFinalProgra/Arduino.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+
+
+namespace ProyectoFinalProgra
+{
+    internal class Arduino
+    {
+        private SerialPort puerto;
+        public Arduino(string nombrePuerto)
+        {
+            puerto = new SerialPort(nombrePuerto, 9500);
+        }
+        public void Enviar(string Mensaje)
+        {
+            if (!puerto.IsOpen)
+            {
+                puerto.Open();
+                Thread.Sleep(2000);
+            }
+            puerto.WriteLine(Mensaje);
+            puerto.Close();
+        }
+    }
+}
